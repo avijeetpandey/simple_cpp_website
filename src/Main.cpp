6e7bfc37009fdbc23a310436d7b5262fa36f1cc6
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-03-29 00:15:52 +0530

    updated code to send html files

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
+#include<crow.h>
+
 using namespace std;
+using namespace crow;
 
 int main() {
 
-	cout << "Hello" << endl;
+	//initializing crow app
+	SimpleApp app;
+
+	CROW_ROUTE(app, "/")([](const request &req, response &res) {
+		ifstream getFile("public/index.html",ifstream::in);
+		if (getFile) {
+			ostringstream contents;
+			contents << getFile.rdbuf();
+			getFile.close();
+			res.write(contents.str());
+			res.end();
+		}
+		else {
+			res.write("Internal server error ! , File not found ");
+		}
+
+	});
+
+	app.port(3000).multithreaded().run();
 
 	return 0;
 }

941b3d36e5e35f72e0f144e6f467c470654a0435
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-03-29 00:53:47 +0530

    added static content serving

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,26 +4,59 @@
 using namespace std;
 using namespace crow;
 
+
+//function to send files
+void sendFile(response &res, string filename, string contentType) {
+	ifstream getFile("public/" + filename, ifstream::in);
+	if (getFile) {
+		ostringstream contents;
+		contents << getFile.rdbuf();
+		getFile.close();
+		res.set_header("Content-Type", contentType);
+		res.write(contents.str());
+	}
+	else {
+		res.code = 404;
+		res.write("Internal Server error , file not found");
+	}
+	res.end();
+}
+
+//function to send html
+void sendHtml(response &res, string filename) {
+	sendFile(res, filename + ".html", "text/html");
+}
+
+//helper function to send images
+void sendImage(response &res, string filename) {
+	sendFile(res, "images/" + filename, "image/jpeg");
+}
+
+//helper function to send scripts
+void sendScripts(response &res, string filename) {
+	sendFile(res, "js/" + filename, "text/javascript");
+}
+
+//helper function to send styles
+void sendCss(response &res, string filename) {
+	sendFile(res, "css/" + filename, "text/css");
+}
+
 int main() {
 
 	//initializing crow server 
 	SimpleApp app;
 
-	CROW_ROUTE(app, "/")([](const request &req,response &res) {
-		
-		ifstream getFile("public/index.html", ifstream::in); //finding the index.html file 
-		if (getFile) {
-			ostringstream contents;
-			contents << getFile.rdbuf(); //extracting the contents of the file 
-			getFile.close();
-			//writting the response to the server
-			res.write(contents.str());
-		}
-		else {
-			res.write("Internal sever error ! File not found ");
-		}
+	CROW_ROUTE(app, "/css/<string>")([](const request &req, response &res, string filename) {
+		sendCss(res, filename);
+	});
 
-		res.end(); //ending the response from the server 
+	CROW_ROUTE(app, "images/<string>")([](const request &req, response &res, string filename) {
+		sendImage(res, filename);
+	});
+
+	CROW_ROUTE(app, "/")([](const request &req,response &res) {
+		sendHtml(res, "index");
 	});
 
 	app.port(3000).multithreaded().run();
